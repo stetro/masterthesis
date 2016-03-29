e8e8dac255cdf517ffcce4fdb20caaf3976cf07c
Steffen Tröster <steffen.troester@googlemail.com>
2016-03-29 17:50:29 +0200

    adding slides for thesis defense

---
+++ slides/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function () {
+	gulp.src('css/*.scss')
+		.pipe(sass())
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('css/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['default'], function() {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("css/*.scss", ['sass']);
+    gulp.watch("index.html").on('change', browserSync.reload);
+});
